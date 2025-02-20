6804b3e8c0341f6b992797a68084586d70b691cf
Oleg K <olegkash@gmail.com>
2025-02-19 21:50:51 -0600

    Made it shorter

--- 1980._Find_Unique_Binary_String_M.py
+++ 1980._Find_Unique_Binary_String_M.py
@@ -6,7 +6,6 @@ class Solution:
         for n in nums:
             num10.add(int(n, 2))
 
-        max_num = pow(2, N)
-        for i in range(max_num):
+        for i in range(N + 1):
             if i not in num10:
                 return "{:0>16b}".format(i)[-N:]
