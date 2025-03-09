fea7e415266aa45433f56c7cc1c569edbb28a5e5
Oleg K <olegkash@gmail.com>
2025-03-08 20:42:40 -0600

    Fixed typo

--- 3208._Alternating_Groups_II_M.py
+++ 3208._Alternating_Groups_II_M.py
@@ -6,11 +6,11 @@ class Solution:
         result = 0
         left = 0
 
-        for rigth in range(1, n + k - 1):
-            if colors[rigth % n] == colors[(rigth - 1) % n]:
-                left = rigth
+        for right in range(1, n + k - 1):
+            if colors[right % n] == colors[(right - 1) % n]:
+                left = right
 
-            if rigth - left + 1 >= k:
+            if right - left + 1 >= k:
                 result += 1
 
         return result
