3ede2f742da215291f3693a5ae66e16137926fc4
Oleg K <olegkash@gmail.com>
2025-03-23 22:02:34 -0500

    Improve solution 3169

--- 3169._Count_Days_Without_Meetings_M.py
+++ 3169._Count_Days_Without_Meetings_M.py
@@ -2,21 +2,14 @@ from typing import List
 
 class Solution:
     def countDays(self, days: int, meetings: list[list[int]]) -> int:
-        free_days = 0
-        latest_end = 0
-
-        # Sort meetings based on starting times
         meetings.sort()
 
+        prev_end = 0
+
         for start, end in meetings:
-            # Add current range of days without a meeting
-            if start > latest_end + 1:
-                free_days += start - latest_end - 1
+            start = max(prev_end + 1, start)
+            length = end - start + 1
+            days -= max(length, 0)
+            prev_end = max(prev_end, end)
 
-            # Update latest meeting end
-            latest_end = max(latest_end, end)
-
-        # Add all days after the last day of meetings
-        free_days += days - latest_end
-
-        return free_days
+        return days
