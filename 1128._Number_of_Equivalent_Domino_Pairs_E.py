2a5a796843d4e52a358b1af1fe6b9eb49dcaced9
Oleg K <olegkash@gmail.com>
2025-05-03 21:32:51 -0500

    Added 1128

---
+++ 1128._Number_of_Equivalent_Domino_Pairs_E.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        num = [0] * 100
+        result = 0
+        for x, y in dominoes:
+            val = x * 10 + y if x <= y else y * 10 + x
+            result += num[val]
+            num[val] += 1
+        return result
